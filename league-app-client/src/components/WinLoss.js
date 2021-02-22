84610a038fa4faa3213cd739fe96064abf1091c0
Inder <inderjeet604@hotmail.com>
2021-02-21 16:57:22 -0800

    Slight adjustment

--- league-app-client/src/components/WinLoss.js
+++ league-app-client/src/components/WinLoss.js
@@ -4,7 +4,7 @@ import { Pie } from 'react-chartjs-2';
 //Displays the win/loss of returned matches
 function WinLoss(props) {
     let winLossRate = props.winLossRate;
-    if(winLossRate !== undefined)
+    if(winLossRate.win !== undefined)
     {
         let winPercent = ((winLossRate.win/(winLossRate.loss+winLossRate.win))*100).toFixed(0);
         const data = {
